dfc86f2141dcb2718694531c4202395ed91e0201
Aniket <vishnoianiket4@gmail.com>
2023-07-16 14:53:01 +0530

    comments added

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,15 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
+// Define the Item interface to represent a single item in the Todo list, with id and name properties.
 interface Item {
   id: string;
   name: string;
 }
 
-const getLocalItems = (): Item[] | (()=> Item[]) => {  
+// Function to get the items from localStorage
+const getLocalItems = (): Item[] | (()=> Item[]) => { 
+  // Check if running on the client-side 
   if (typeof window !== 'undefined') {
   let list = localStorage.getItem("list");
   if (list) {
@@ -18,6 +21,7 @@ const getLocalItems = (): Item[] | (()=> Item[]) => {
    else {
     return [];
    }
+    // Return an empty function if localStorage is not available
    return()=>[]
 };
 
@@ -28,10 +32,13 @@ export default function Home() {
   const [toggle, setToggle] = useState<boolean>(true);
   const [editItem, setEdititem] = useState<string | null>(null);
 
+  // Handle input change
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
+
+  // Add or edit an item
   const addData = () => {
     if (!value) {
       alert("Please Enter the data");
@@ -57,11 +64,13 @@ export default function Home() {
     }
   };
 
+  // Delete an item function
   const deleteItem = (index: string) => {
     const updateItems = add.filter((elem) => index !== elem.id);
     setAdd(updateItems);
   };
 
+  // Edit an item function
   const editItems = (ind: string) => {
     const editItem = add.find((elem) => elem.id === ind);
     if (editItem) {
@@ -71,6 +80,7 @@ export default function Home() {
     }
   };
 
+  // Update localStorage when 'add' state changes
   useEffect(() => {
     localStorage.setItem("list", JSON.stringify(add));
   }, [add]);
@@ -84,6 +94,8 @@ export default function Home() {
           <input className="border-solid pl-4 pr-4 shadow-md font-[Inter] font-medium text-base opacity-100 bg-[#262626d9]
            text-[#dedede] border-[#262626d9] border-2 w-[566px] md:w-96 sm:w-80 xs:w-60 xm:w-52 rounded-md" value={value} onChange={handleOnChange}/>
           <a className="flex justify-center items-center p-2  px-8 sm:px-5 bg-[#3aa1ddd6] bg-opacity-100 rounded-md hover:cursor-pointer">
+           
+            {/* Toggle between 'Add' and 'Edit' based on the 'toggle' state */}
            {toggle ? ( 
             <h1 className="font-roboto font-medium text-560 text-[Inter] text-[#fffefe] text-20" onClick={addData} >
               Add
@@ -99,12 +111,14 @@ export default function Home() {
 
 
       <div className="  flex flex-col gap-3 xxs:gap-2 xm:gap-1 mt-20 xxs:mt-14 ">
+        {/* Render the list of items */}
         {add.map((element)=>{
           return(
             <div className=" flex border-solid pl-4 pr-4 items-center shadow-md font-[Inter] font-medium text-base bg-[#262626d9] text-[#dedede]
              border-[#262626d9] border-2 w-[666px] md:w-[484px] sm:w-96 xs:w-80 xm:w-72 rounded-md" key={element.id}  >
               <h1 className="break-all w-[544px] p-3 md:w-[375px] sm:w-72 xs:w-56 sm:text-sm xs:text-xs xm:w-48 ">{element.name}</h1>
               <div className="flex gap-2 pl-6">
+                {/* Delete button */}
                 <a>
                   <Image
                     alt="Delete"
@@ -115,6 +129,7 @@ export default function Home() {
                     onClick={()=>deleteItem(element.id)}
                   />
                 </a>
+                {/* Edit button */}
                 <a>
                   <Image alt="Edit" src="/img/edit.png" width={100} height={100} className="w-5 h-5 xs:w-4 xs:h-4" onClick={()=>editItems(element.id)} />
                 </a>
